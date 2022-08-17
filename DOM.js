3487684963cc201e05aecfdb41209927335c597d
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-08-18 02:29:35 +0600

    new

--- DOM.js
+++ DOM.js
@@ -206,3 +206,79 @@ mainContainer.appendChild(sectionDress);
 document.createElement('tagName')
 tagName.innerText = 'newText'
 
+//--------------------------------------------------------------
+//-----------------------addEventListener-----------------------
+//--------------------------------------------------------------
+
+// option 1: directly set on the HTML element
+// <button onclick="console.log(65)">Another button</button>
+
+//  option 2: add onclick function on the html element
+// IMPORTANT: We will use this
+// <button onclick="makeRed()">Make Red</button>
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+//  option: 3 
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+// option: 3 another 
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple'
+}
+
+// option: 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option: 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+});
+
+// option: 4 Final
+// IMportant: We will use this sometimes
+// document.getElementById('make-goldenrod').addEventListener('click', function(){})
+document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod';
+})
+
+//-------------------most use------------------
+
+{/* <button onclick="handleOnClick()">Handle Event by Add onclick Function</button> */ }
+<button id="event-listener">Handle event by addEventListener</button>
+{/* <script> */ }
+// option 1
+function handleOnClick() {
+    const handlerStatus = document.getElementById('handler-status');
+    handlerStatus.innerText = 'Handled by function attached on onclick attribute';
+}
+// option 2
+document.getElementById('event-listener').addEventListener('click', function () {
+    const handlerStatus = document.getElementById('handler-status');
+    handlerStatus.innerText = 'Text updated by add Event listener button'
+})
+
+// option 2: recap
+document.getElementById('btn-update').addEventListener('click', function () {
+    const inputField = document.getElementById('input-field');
+    const inputText = inputField.value;
+
+    const p = document.getElementById('input-text-display');
+    p.innerText = inputText;
+    inputField.value = '';
+})
+{/* </script> */ }
